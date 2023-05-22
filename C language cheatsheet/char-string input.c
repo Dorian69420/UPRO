ee070b3ee123e765b81f9054c1c42549f5003222
Dorian Jerčić <dorian.jercic@gmail.com>
2023-05-22 23:05:38 +0200

    Update char-string input.c

---
+++ C language cheatsheet/char-string input.c
@@ -0,0 +1,4 @@
+char znak, rijec[20], recenica[20];
+scanf(" %c", &znak); // unesi znak; razmak je na početku da prekočimo prethodni ENTER po potrebi
+scanf("%s", rijec); // unesi riječ;
+scanf("%[^\n]", recenica); // unesi rečenicu; čita sa konzole dok se ne pritisne ENTER
